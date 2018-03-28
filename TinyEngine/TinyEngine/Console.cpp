571b9f0dee9bc6a7ca4d485c706a800b6ba88a2e
FabioBrigidi <fabio.brigidi@gmail.com>
2018-03-28 13:47:13 +0200

    Modified console

--- TinyEngine/TinyEngine/Console.cpp
+++ TinyEngine/TinyEngine/Console.cpp
@@ -2,35 +2,20 @@
 #include "Console.h"
 #include <map>
 
-typedef void(*CommandFunction)();
-
-struct CommandDefinition
-{
-	std::string Command			= "";
-	std::string Description		= "";
-	CommandFunction Function    = nullptr;
-
-};
-
-typedef std::map<std::string, CommandDefinition>					_CmdMap;
-typedef std::map<std::string, CommandDefinition>::const_iterator	_CmdMapIter;
-
 class idConsoleLocal : public idConsole
 {
 public:
 	virtual	void		Init(void);
 	virtual void		Shutdown(void);
-	virtual	bool		ProcessEvent(const std::string & Cmd) const;
+
+
+	virtual	bool		ProcessCommand(const std::string & Cmd) const;
+	virtual bool		AddCommand(const std::string & Cmd, void(*Function)(), const std::string & Description);
 
 	_CmdMapIter			GetCommandsIterBegin() const;
 	_CmdMapIter			GetCommandsIterEnd() const;
 
 private:
-	bool				AddCommand(const std::string & Cmd, void(*Function)(), const std::string & Description);
-	
-
-	//============================
-	
 	void				ListCommands();
 
 	//============================
--- TinyEngine/TinyEngine/Console.h
+++ TinyEngine/TinyEngine/Console.h
@@ -1,6 +1,19 @@
 #pragma once
 #include <string>
 
+typedef void(*CommandFunction)();
+
+struct CommandDefinition
+{
+	std::string Command = "";
+	std::string Description = "";
+	CommandFunction Function = nullptr;
+
+};
+
+typedef std::map<std::string, CommandDefinition>					_CmdMap;
+typedef std::map<std::string, CommandDefinition>::const_iterator	_CmdMapIter;
+
 class idConsole
 {
 public:
@@ -9,6 +22,7 @@ public:
 	virtual void	Shutdown(void) = 0;
 
 	virtual bool	ProcessEvent(const std::string & Cmd) const = 0;
-
+	virtual bool	AddCommand(const std::string & Cmd, void(*Function)(), const std::string & Description);
+	virtual bool	RemoveCommand(const std::string & Cmd);
 };
 
